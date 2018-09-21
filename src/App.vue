a162df30649c483593aa9a2c46f61f32fa3845b9
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-09-21 20:17:37 +1200

    added components structure and a valid representation of basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-    <h1>Hello World!</h1>
+    <!-- <app-server-status></app-server-status> -->
+
+    <div class="container">
+      <app-header></app-header>
+      <hr>
+      <div class="row">
+        <app-servers></app-servers>
+        <app-server-details></app-server-details>
+      </div>
+      <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
+import Header from "./components/Shared/Header.vue";
+import Footer from "./components/Shared/Footer.vue";
+import Servers from "./components/Server/Servers.vue";
+import ServerDetails from "./components/Server/ServerDetails.vue";
 
+export default {
+  //local registration
+  components: {
+    "app-header": Header,
+    "app-servers": Servers,
+    "app-server-details": ServerDetails,
+    "app-footer": Footer
+  }
+};
 </script>
-
-<style>
-</style>
---
+++ src/components/Shared/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <app-server v-for="server in 5"></app-server>
+</div>
+</template>
+
+<script>
+import ServerStatus from "../Server/ServerStatus.vue";
+
+export default {
+  components: {
+    "app-server": ServerStatus
+  }
+};
+</script>
+
