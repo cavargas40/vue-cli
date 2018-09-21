bb4e1ad0b829c28866e6f4f38076ed3ce863ec85
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-09-22 09:54:56 +1200

    added suboptimal solution to pass content between components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <app-user></app-user>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 ">
+      <app-quote :quote="'Am wonderful quote!'"></app-quote>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import appUser from "./components/User.vue";
+import appQuote from "./components/Quote.vue";
 
 export default {
   components: {
-    appUser
+    appQuote
   }
 };
 </script>
