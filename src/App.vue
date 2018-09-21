6300fe753447c39d5bf18e5defbbae12a097a3db
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-09-22 10:06:49 +1200

    the style can be changed on the slot parent or child

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col-xs-12 col-sm-12 ">
       <app-quote>
-          <h2>The Quote</h2>
+          <h2>{{ quoteTitle }}</h2>
           <p>A wonderful quote!</p>
       </app-quote>
     </div>
@@ -15,8 +15,20 @@
 import appQuote from "./components/Quote.vue";
 
 export default {
+  data: function() {
+    return {
+      quoteTitle: "The Quote"
+    };
+  },
   components: {
     appQuote
   }
 };
 </script>
+
+<style scoped>
+h2 {
+  color: red;
+}
+</style>
+
