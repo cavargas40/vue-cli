3839fc8f82e412c9ce482d33f26ad9a6fd3fd29e
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-09-22 09:59:18 +1200

    pass content with slots

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
 <div class="container">
   <div class="row">
     <div class="col-xs-12 col-sm-12 ">
-      <app-quote :quote="'Am wonderful quote!'"></app-quote>
+      <app-quote>
+          <h2>The Quote</h2>
+          <p>A wonderful quote!</p>
+      </app-quote>
     </div>
   </div>
 </div>
